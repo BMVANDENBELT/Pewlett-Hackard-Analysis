e6be8d0a9b5fae80692054a53c323e2ff6ac6ba5
Braeden Vandenbelt <braedenvandenbelt@Braedens-MacBook-Air.local>
2022-08-25 14:43:17 -0400

    7.2.2

--- queries.sql
+++ queries.sql
@@ -6,7 +6,8 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
-CREATE TABLE employees (emp_no INT NOT NULL,
+CREATE TABLE employees (
+	 emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -16,7 +17,7 @@ CREATE TABLE employees (emp_no INT NOT NULL,
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -34,4 +35,23 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM Departments;
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL, 
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+SELECT * FROM departments;
